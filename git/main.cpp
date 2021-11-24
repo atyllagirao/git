ee61a1913c7e58222f42d8b99ac00c5a4f5778ea
atylla girao <atyllagirao@hotmail.com>
2021-11-24 12:13:17 -0300

    Initial Commit

---
+++ git/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  git
+//
+//  Created by atylla on 24/11/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
